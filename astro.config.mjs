dcb58c5c2552634aef4f22197ae55b522002219b
Jeffrey A <agyepong99@gmail.com>
2024-09-13 00:26:57 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 import playformCompress from '@playform/compress';
 
-// https://astro.build/config
+
 export default defineConfig({
   site: 'https://mintmantis.ca/',
   integrations: [
