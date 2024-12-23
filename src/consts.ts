f15ec24a7c10f7d3fbf030a81dccbfcec4198578
Jeffrey A <agyepong99@gmail.com>
2024-12-23 16:42:00 -0700

    meta tag

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mint Mantis | Creative Agency, Artist Management, Smart Publishing';
+export const SITE_TITLE = 'Mint Mantis | Smart Publishing, Artist Management, Sponsorships';
 export const SITE_DESCRIPTION = 'Mint Mantis empowers artists with the resources, skills, and guidance to turn their art into a thriving business.';
